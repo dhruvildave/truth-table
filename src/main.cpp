7a56eb795ebb41e90052fb9df72e31f29b2293d6
Dhruvil Dave <dhruvil.dave@outlook.com>
2019-02-03 20:59:54 +0530

    Initial Version

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,20 @@
 
 int main(void)
 {
-    // std::cout << "::Truth Table::" << '\n';
+    // Use the "binary" namespace for binary numbers
+    // Use the "tables" namespace for truth tables
+    
+    std::cout << "::Truth Table::" << '\n';
 
-    // std::cout << "Enter the number of bits: ";
-    // unsigned int b{binary::bit_init()};
+    std::cout << "Enter the number of bits: ";
+    unsigned int b{binary::bit_init()};
 
-    // table::AND(b);
-    // std::cout << '\n';
-    // table::IOR(b);
-    // std::cout << '\n';
-    // table::XOR(b);
-    // std::cout << '\n';
+    table::AND(b);
+    std::cout << '\n';
+    table::IOR(b);
+    std::cout << '\n';
+    table::XOR(b);
+    std::cout << '\n';
 
     std::vector<unsigned int> v{binary::new_number_in()};
     for (auto i:v)
